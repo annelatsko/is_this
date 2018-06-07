140925647a7a36c25d388b75a05dee779cd03146
Linden Holt <ace.holt@gmail.com>
2018-06-07 14:01:18 -0400

    git ignore, python modules added

---
+++ imageScrape/root/nested/imageScrape.py
@@ -0,0 +1,15 @@
+'''
+Created on Jun 7, 2018
+
+@author: Linden Holt
+'''
+from bs4 import BeautifulSoup
+import requests
+import re
+import urllib2
+import os
+import argparse
+import sys
+import json
+
+print("I work")
